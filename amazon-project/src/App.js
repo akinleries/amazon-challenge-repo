081aadd1cf935ed87c853aa45c5c9aab2b09a952
lasisi Ezekiel <ezekielakintunde18@gmail.com>
2021-10-01 21:45:22 +0100

    first commit

---
+++ amazon-project/src/App.js
@@ -0,0 +1,12 @@
+ 
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello guys, lets build an amazon store for semicolon africa  </h1>
+    </div>
+  );
+}
+
+export default App;
