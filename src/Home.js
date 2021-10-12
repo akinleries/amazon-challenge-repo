deaa528ff0b8cd5c1e5ef63d6921c5285f86305a
lasisi Ezekiel <ezekielakintunde18@gmail.com>
2021-10-12 06:47:48 +0100

    slight changes

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import './Home.css'
 import Product from './Product'
-import City from './images/city.jpg'
+//import City from './images/city.jpg'
 
 
 const Home = () => {
   return (
     <div className = "home">
         <div className="home-container">
-
-         <img className = "home__image" src={City} alt="" />
+{/* 
+         <img className = "home__image" size ={150} src={City} alt="" /> */}
+         <img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2021/img/Events/Holiday/EpicDailyDeals/LandingPage/HOL21_EDD_Phase4_hero-banner_short_desktop_1500x150.jpg" alt="Epic Daily Deals"/>
 
       <div className="home__row">
             <Product/>
