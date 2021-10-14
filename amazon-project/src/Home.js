a11b0a70b129d0b4b8d4211ce371fd2f363deb69
lasisi Ezekiel <ezekielakintunde18@gmail.com>
2021-10-14 04:55:27 +0100

    adding more functionalities

--- amazon-project/src/Home.js
+++ amazon-project/src/Home.js
@@ -1,35 +1,75 @@
 import React from 'react'
 import './Home.css'
 import Product from './Product'
+import AdertImage from './images/market_image.jpg'
+import Chair from './images/amazon_chair.jpg'
 //import City from './images/city.jpg'
 
 
 const Home = () => {
   return (
+   <div>
+      <div className = "first__text">
+        <marquee behavior = "scroll" direction = "left" width = "100%">Welcome to amazon Online Shopping</marquee>
+      </div>
     <div className = "home">
+  
         <div className="home-container">
+
 {/* 
          <img className = "home__image" size ={150} src={City} alt="" /> */}
-         <img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2021/img/Events/Holiday/EpicDailyDeals/LandingPage/HOL21_EDD_Phase4_hero-banner_short_desktop_1500x150.jpg" alt="Epic Daily Deals"/>
-
+         {/* <img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2021/img/Events/Holiday/EpicDailyDeals/LandingPage/HOL21_EDD_Phase4_hero-banner_short_desktop_1500x150.jpg" alt="Epic Daily Deals"/> */}
+          <img className = "home__image" src={AdertImage} alt="" />
       <div className="home__row">
-            <Product/>
-            <Product/>
+        
+            <Product 
+            title = "seat comfortably"
+            price = {12.02}
+            rating = {5}
+            image = {Chair}
+            />
+            <Product
+             title = "seat comfortably"
+             price = {12.02}
+             rating = {5}
+             image = {Chair}
+            />
       </div>
 
       <div className="home__row">
-            <Product/>
-            <Product/>
-            <Product/>
+            <Product
+             title = "seat comfortably"
+             price = {12.02}
+             rating = {5}
+             image = {Chair}
+            />
+            <Product
+             title = "seat comfortably"
+             price = {12.02}
+             rating = {5}
+             image = {Chair}
+            />
+            <Product
+             title = "seat comfortably"
+             price = {12.02}
+             rating = {5}
+             image = {Chair}
+            />
       </div>
 
     <div className="home__row">
-           <Product/>
+           <Product
+            title = "seat comfortably"
+            price = {12.02}
+            rating = {5}
+            image = {Chair}
+           />
     </div>
           
 
     </div>  
     </div>
+   </div>
   )
 }
 
--- amazon-project/src/Product.js
+++ amazon-project/src/Product.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import './Product.css'
 
-const Product = () => {
+const Product = ({title, price, rating, image}) => {
   return (
     <div className= "product">
       <div className="product__info">
-        <p>Lorem ipsum dolor sit amet consectetur adipis</p>
+        <p>{title}</p>
         <div className="product__price">
           <p>
             <small>$</small>
-            <strong>18.00</strong>
+            <strong>{price}</strong>
           </p>
         </div>
 
-      <div className="product__rating">
-        
-      </div>  
+      <div className="product__rating">{
+        Array(rating).fill().map(( _, i) => (
+          <p>*</p>
+        ))
+      }</div>  
       </div>
-      <img src="" alt="" />
-    <button>add to cart</button>  
+      <img className = "product__image" src={image} alt="" />
+      <div className="add_to_cart_button">add to cart</div> 
     </div>
   )
 }
