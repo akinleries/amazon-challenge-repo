e88706df4b96ee7e94163f7760423c702d1d31d3
lasisi Ezekiel <ezekielakintunde18@gmail.com>
2021-10-23 23:41:17 +0100

    adding more functionalities

--- amazon-project/src/Subtotal.js
+++ amazon-project/src/Subtotal.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import './Subtotal.css'
 import CurrencyFormat from 'react-currency-format'
+import { useStateValue } from './StateProvider'
+ 
 
 const Subtotal = () => {
+
+  const [{basket}, dispatch] = useStateValue();
     return (
         <div className = "subtotal">
-             <CurrencyFormat />
+             <CurrencyFormat 
+             renderText = {(value) => (
+               <div>
+                     <p> Subtotal ({basket.length} items) : <strong>{basket?.reduce((amount, item) => item.price + amount, 0 )}</strong></p>
+                 <small className = "subtotal__gift"><input type="checkbox" />This order contains gift </small>
+               </div>
+             )}
+             decimalScale = {2}
+             value = {0}
+             displayType = {"text"}
+             thousandSeparator = {true}
+             prefix = {"$"}
+             />
+             <div className="checkout__button">proceed to checkout</div>
         </div>
     )
 }
